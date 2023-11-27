abf95429c79913a8ea712997e058e469b3477f77
napoknot21 <napoknot21@protonmail.com>
2023-11-27 15:40:58 +0100

    Code improvements

--- TPs/TP2/solution/tp2.py
+++ TPs/TP2/solution/tp2.py
@@ -71,48 +71,26 @@ def fvec(h,theta,xcible):
 #         h and xcible = problem data
 # Output: thetanext    = estimated new (theta^(k+1))
 def newton_step(h,xcible,theta):
-    """
+
     # initialization
     y=np.array([0.,0.,0.]); 
     d_theta=np.array([0.,0.,0.]);
 
-    # on commence par évaluer la jacobienne
-    J=jacobienne(h,theta);
-    f=fvec(h,theta ,xcible); # Composantes du second membre (f(x))
+    # Evaluate the Jacobian
+    J = jacobienne(h, theta)
+    f = fvec(h, theta, xcible)  # Components of the right-hand side (f(x))
     
     # Compute d_theta using the LU deomposition 
     P, L, U = scipy.linalg.lu(J); # utilisation de la decomposition LU de python (be careful with P)
     # Python gives P.L.U=A
     # We have A.theta=-f, P.L.U=-f but we solve L.U=P^{T}.(-f)
     #   P transpose = P inverse
-    
-    # Résolution du système:
-    # firtly we need to invert the line of f considering matrix P
-    F2 = -np.dot(np.transpose(P),f) #F2=-P*f;
-    
-    # We need to inverse L.U.theta = F2 to find d_theta
-    # find the solution of the two systems Lz = b and then Ux=z
-    # Solve the system Lz = F2 for z
-    z = scipy.linalg.solve(L, F2)
 
-    # Now solve U.d_theta = z for d_theta
-    d_theta = scipy.linalg.solve(U, z)
-    
-    # incrémentation of d_theta
-    thetanext = theta + d_theta;        
-    
-    return(thetanext)
-    """
-    # Evaluate the Jacobian
-    J = jacobienne(h, theta)
-    f = fvec(h, theta, xcible)  # Components of the right-hand side (f(x))
-    
-    # Compute d_theta using LU decomposition
-    P, L, U = scipy.linalg.lu(J)  # Use Python's LU decomposition (be careful with P)
-    
     # We solve L.U.d_theta = -P.T*f
     F2 = -np.dot(np.transpose(P), f)  # F2 = -P*f
     
+    # We need to inverse L.U.theta = F2 to find d_theta
+    # find the solution of the two systems Lz = b and then Ux=z
     # Solve the system Lz = F2 for z
     z = scipy.linalg.solve(L, F2)
     
@@ -128,43 +106,8 @@ def newton_step(h,xcible,theta):
 # Problem data: h and xcible
 # Output: theta = estimated angle
 def newton(h,xcible,theta0):
-    """
     # Set the tolerance level
-    epsilon = 1e-12
-
-    #initialization
-    theta = np.copy(theta0);
-       
-    # include a criteria in the loop
-    
-    # Initialize the increment to a large value
-    increment = np.inf
-    # Counter for iterations
-    iteration = 0
-    
-    # Newton's method loop
-    while increment > epsilon:
-
-        # Calculate the new estimate for theta
-        thetanext = newton_step(h, xcible, theta)
-        
-        # Calculate the increment
-        increment = scipy.linalg.norm(thetanext - theta)
-        
-        # Update theta
-        theta = thetanext
-        
-        # Update the iteration counter
-        iteration += 1
-        
-        # Optional: print iteration number and increment
-        print(f"Iteration {iteration}: Increment = {increment}")
-    
-    # When the increment is sufficiently small, return theta
-    return theta
-    """
-    # Set the tolerance level
-    epsilon = 1e-12
+    #epsilon = 1e-12
 
     # Initialize theta with the initial guess
     theta = np.copy(theta0)
@@ -195,6 +138,7 @@ def newton(h,xcible,theta0):
     return theta
 
 
+epsilon = 1e-12
 
 # arms length
 h=np.array([1.,1.,1.]);
